a04acb21eddf89210dc44a6059bdc7b53397973d
OrtizIgnacioA <nacho.agus.ortiz@gmail.com>
2020-06-23 14:23:55 -0300

    Se crea Formulario
    Se crea formulario crear-persona

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
 import { CrearPersonaComponent } from './componentes/crear-persona/crear-persona.component';
 import { ConsultarPersonasComponent } from './componentes/consultar-personas/consultar-personas.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -13,7 +18,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     CrearPersonaComponent,
     ConsultarPersonasComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/componentes/crear-persona/crear-persona.component.ts
+++ src/app/componentes/crear-persona/crear-persona.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-crear-persona',
   templateUrl: './crear-persona.component.html',
-  styleUrls: ['./crear-persona.component.css']
+  styleUrls: ['./crear-persona.component.css'],
 })
 export class CrearPersonaComponent implements OnInit {
+  /* crearPersonaForm: FormGroup; */
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
+  crearPersonaForm = this.fb.group({
+    nombre: [''],
+    apellido: [''],
+    edad: [''],
+    direccion: [''],
+  });
+
+  ngOnInit(): void {}
+
+  submit() {
+    debugger;
+    this.crearPersonaForm.value;
   }
-
 }
